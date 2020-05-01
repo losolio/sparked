1b600c637d8db61b91ac6eee31d598927e6d9e68
Ole Kristian Losvik <ole@losol.no>
2020-05-01 23:04:40 +0200

    Added fhir patient search

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import FHIR from 'fhirclient';
 import './App.css';
 
 function App() {
+  const [patients, setPatients] = useState({ entry: [] });
+
+  useEffect(async () => {
+    const fetchPatients = async () => {
+      const client = FHIR.client({
+        serverUrl: "http://spark.kufu.no/fhir"
+      });
+      client.request("Patient")
+        .then(data =>
+          setPatients(data)
+        );
+    }
+    fetchPatients();
+  }, []);
+
   return (
     <div className="App">
-      <p>asdf</p>
+      <p>Patients</p>
+      <ul>
+        {patients.entry.map(item => (
+          <li>Name: </li>
+        ))}
+      </ul>
     </div>
   );
 }
